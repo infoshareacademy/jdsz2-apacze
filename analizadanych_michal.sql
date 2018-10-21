8417deb87939110c24e7168fa1ddf21555cab893
mateuszandzelak01 <m.andzelak@gmail.com>
2018-10-21 09:57:47 +0200

    DODANIE PLIKOW

---
+++ analizadanych_michal.sql
@@ -0,0 +1,23 @@
+Co to jest wysoka roznica:
+
+
+1. Top 10 najwiekszych roznic nominalnych
+ / Jakub
+2. 10-15% najwiekszych roznic procentowych / Mateusz
+3. Liczba wnioskow powyzej mediany
+ / Liliana
+4. zbadaæ skrajne wartoœci 5% / Micha³ 
+
+
+
+Analiza:
+
+1.rozklad czasu
+
+2.wplyw partnera
+
+3.wplyw kod kraju
+
+4.wplyw klient biznesowy
+
+5.wplyw liczba pasazerow
