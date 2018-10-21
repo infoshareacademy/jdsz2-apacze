5b7b4ac089e626fc275d28f82158f7047fa63aed
Liliana Gmerek <erg0@gmx.com>
2018-10-21 10:19:55 +0200

    JIRA - 21 lista wnioskow powyzej mediany oraz liczba tych wnioskow

--- analizadanych_liliana.sql
+++ analizadanych_liliana.sql
@@ -4,9 +4,46 @@ Co to jest wysoka roznica:
 1. Top 10 najwiekszych roznic nominalnych
  / Jakub
 2. 10-15% najwiekszych roznic procentowych / Mateusz
-3. Liczba wnioskow powyzej mediany
- / Liliana
-4. zbadaæ skrajne wartoœci 5% / Micha³ 
+3. Liczba wnioskow powyzej mediany / Liliana
+
+---3.1. Lista wnioskow powyzej mediany
+with roznice_rekompensat
+as
+(
+select id, kwota_rekompensaty, kwota_rekompensaty_oryginalna,
+abs(kwota_rekompensaty-kwota_rekompensaty_oryginalna) as roznica
+ from wnioski
+ where abs(kwota_rekompensaty-kwota_rekompensaty_oryginalna) >0
+),
+mediana AS(
+select
+percentile_disc(0.5) within group (order by roznica) as mediana
+from roznice_rekompensat
+)
+select * from roznice_rekompensat, mediana
+where roznica >=mediana
+order by roznica desc
+
+----3.2. Liczba wnioskow powyzej mediany  #2131
+with roznice_rekompensat
+as
+(
+select id, kwota_rekompensaty, kwota_rekompensaty_oryginalna,
+abs(kwota_rekompensaty-kwota_rekompensaty_oryginalna) as roznica
+ from wnioski
+ where abs(kwota_rekompensaty-kwota_rekompensaty_oryginalna) >0
+),
+mediana AS(
+select
+percentile_disc(0.5) within group (order by roznica) as mediana
+from roznice_rekompensat
+)
+
+select count(1) from roznice_rekompensat,mediana
+where roznica >=mediana
+
+
+4. zbadaï¿½ skrajne wartoï¿½ci 5% / Michaï¿½ 
 
 
 
