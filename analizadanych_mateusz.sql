96cf7bc9001749d60b2594290d3f1ae12248e1df
mateuszandzelak01 <m.andzelak@gmail.com>
2018-11-18 12:38:46 +0100

    pliki CSV z danymi

--- analizadanych_mateusz.sql
+++ analizadanych_mateusz.sql
@@ -37,6 +37,8 @@ select rokmsc,sum(wyplacone),max(wyplacone),count(1) from czas
 group by rokmsc
 order by 4 desc
 
+----
+
 -----------Analiza: 2.2.wplyw partnera
 with partnerzy as (
   select partner,kwota_rekompensaty as wyplacone,kwota_rekompensaty_oryginalna,
@@ -104,21 +106,23 @@ and kwota_rekompensaty <> 0
 --and 100.0*(kwota_rekompensaty_oryginalna-kwota_rekompensaty)/kwota_rekompensaty > 100
 order by 5 desc
 ),
-wnioski_nasze as (select w.id,w.kod_kraju,w.opoznienie,
+wnioski_nasze as (select w.id,w.kod_kraju,
                          sum(roznica) as suma_roznica1,max(roznica) as max_roznica2,count(w.id) as liczba
 from partnerzy p
 join wnioski w on p.id = w.id
 where kod_kraju in ('ZZ') --and typ_podrozy is null
      --and to_char(data_utworzenia,'YYYY-MM') > '2017-06'
      and opoznienie like '%> 3h%'
-group by w.id,w.kod_kraju,w.opoznienie
+group by w.id,w.kod_kraju
 order by 1 desc)
 
-select wn.kod_kraju,wn.opoznienie,count(1),kod_wyjazdu,kod_przyjazdu
+select wn.kod_kraju,count(1),kod_wyjazdu,kod_przyjazdu
 from wnioski_nasze wn
 join podroze p2 on wn.id = p2.id_wniosku
 join szczegoly_podrozy sp on p2.id = sp.id_podrozy
-group by wn.kod_kraju,wn.opoznienie,kod_wyjazdu,kod_przyjazdu
+group by wn.kod_kraju,kod_wyjazdu,kod_przyjazdu
+order by 2 desc
+limit 10
 
 ---- Komentarz: Najwiecej róznic wystepuje w lotach pomiedzy lotniskami HHN-FRA,
 ---- okazuje sie ze obydwa lotniska znajduja sie w Niemczech najprawdopodniej blad w bazie,
@@ -126,7 +130,8 @@ group by wn.kod_kraju,wn.opoznienie,kod_wyjazdu,kod_przyjazdu
 
 -----Potwierdzenie wniosków
 
-with wnioski_spr as (select w.id
+with wnioski_spr as
+    (select w.id
 from wnioski w
 join podroze p on w.id = p.id_wniosku
 join szczegoly_podrozy s2 on p.id = s2.id_podrozy
@@ -139,10 +144,28 @@ join szczegoly_podrozy s2 on p.id = s2.id_podrozy
 group by w.id
 order by 2 desc
 ;
-select w.id,s2.kod_wyjazdu,s2.kod_przyjazdu,s2.data_wyjazdu,s2.data_utworzenia,s2.data_aktualizacji
+---pivot
+
+with wnioski_spr as
+    (select w.id
 from wnioski w
 join podroze p on w.id = p.id_wniosku
 join szczegoly_podrozy s2 on p.id = s2.id_podrozy
-where w.id=2048230
+where kod_wyjazdu = 'HHN' and kod_przyjazdu = 'FRA' and stan_wniosku like 'wypl%'
+group by w.id),
+
+liczba_spr as (
+     select w.id,count(1) as liczba
+from wnioski_spr w
+join podroze p on w.id = p.id_wniosku
+join szczegoly_podrozy s2 on p.id = s2.id_podrozy
+group by w.id
+order by 2 desc)
+
+select case when liczba > 1 then 'laczony_lot' else 'pojedynczy_lot' end as rodzaj_lotu,count(liczba)
+from liczba_spr
+group by liczba
+
+---
 
 
