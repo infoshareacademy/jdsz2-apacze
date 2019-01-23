709316898604395b576c4ae2ede09b5f69e51526
Michal Kitowski <michal.kitowski@gmail.com>
2019-01-23 21:30:48 +0100

    obudowanie funkcji czesc pierwsza, jira JDSZ2AP-28

--- projekt_python/python_projekt.py
+++ projekt_python/python_projekt.py
@@ -1,24 +1,58 @@
+"""
+http://home.agh.edu.pl/~chwiej/mn/generatory_2015.pdf
+http://www.if.pw.edu.pl/~agatka/numeryczne/wyklad_12.pdf
+
+1. Wygenerowaæ liczby za pomoc¹ "Generatora liczb z rozk³adu normalnego" dwoma metodami
+- Metod¹ Boxa-Mullera (funkcja)
+- Metod¹ Centralnego twierdzenia granicznego(funkcja)
+
+2. Przetestowaæ czy generetory, generuj¹ w taki sam sposób jak wbudowany w Pythona generator liczb z rozk³adu normalnego
+
+3. Wykonaæ testy statystyczne:
+- Test Shapiro-Wilka
+- Test Kolmogorov–Smirnov
+- Test Monte Carlo
+"""
+
+import logging
 import pandas as pd
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
 from numpy import random, sqrt, log, sin, cos, pi
 
+logging.basicConfig()
 
-###Box-Muller
 
-# transformation function
-def box_muller(u1,u2):
-  z1 = sqrt(-2*log(u1))*cos(2*pi*u2)
-  z2 = sqrt(-2*log(u1))*sin(2*pi*u2)
-  return z1,z2
+def box_muller_transform(inp_u1, inp_u2):
+    """
+    Box Muller transformation function
+    :param inp_u1: numpy.ndarray  or numeric
+    :param inp_u2: numpy.ndarray or numeric
+    :return: transformed numpy.ndarray or numeric
+    """
+
+    try:
+        out_z1 = sqrt(-2 * log(inp_u1)) * cos(2 * pi * inp_u2)
+        out_z2 = sqrt(-2 * log(inp_u1)) * sin(2 * pi * inp_u2)
+    except (TypeError, AttributeError) as msg:
+        logging.warning(msg='Error {0}, \nexiting program'.format(msg))
+        exit(0)
+
+    return out_z1, out_z2
+
+
+
+
+
+
 
 # uniformly distributed values between 0 and 1
 u1 = random.rand(4000)
 u2 = random.rand(4000)
 
 # run the transformation
-z1,z2 = box_muller(u1,u2)
+z1,z2 = box_muller_transform(u1,u2)
 
 #print(z1,z2)
 #plt.hist(z1)
