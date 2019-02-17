b417faccb088584c5159e04dc39f278b0f107d4d
mateuszandzelak01 <m.andzelak@gmail.com>
2019-02-17 21:34:54 +0100

    plots

--- project_ML_kicstarter/project_ML.py
+++ project_ML_kicstarter/project_ML.py
@@ -1,20 +1,47 @@
 import pandas as pd
 import numpy as np
+import string
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-pd.set_option('display.max_columns', 12)
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
+#### Data_set
+pd.set_option('display.max_columns', 12)
 df = pd.read_csv('data\ks-projects-201801.csv')
+
+### Research
+plt.subplot(221, title='Main category')
+df['main_category'].value_counts().plot.bar()
+plt.subplot(222, title='Currency')
+df['currency'].value_counts().plot.bar()
+plt.subplot(223, title='Country')
+df['country'].value_counts().plot.bar()
+plt.subplot(224, title='State')
+df['state'].value_counts().plot.bar()
+plt.show()
+
+### Data preparation
 categorical_columns = ['category', 'main_category', 'country', 'currency']
 df = pd.get_dummies(df, columns=categorical_columns)
-df = df.drop(columns=['ID', 'name', 'pledged', 'usd pledged', 'usd_pledged_real', 'usd_goal_real'],axis=1)
+df = df.drop(columns=['ID', 'name', 'pledged', 'usd pledged', 'usd_pledged_real', 'backers'], axis=1)
+print(df.shape)
+print(df.columns)
 
-X = df.drop(columns=['state'],axis=1)
-y = df[['state']]
-#### 1. Preprocessing
-print(X.shape)
-print(X.head(5))
 
+
+#### 2.
+#X = df.drop(columns=['state'],axis=1)
+#y = df[['state']
